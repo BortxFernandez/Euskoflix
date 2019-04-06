90dc8aece66c5b157a5a164257ca06710c669a25
mikelmerayo <48220075+mikelmerayo@users.noreply.github.com>
2019-04-06 15:24:30 +0200

    Add files via upload

--- src/Principal/Euskoflix.java
+++ src/Principal/Euskoflix.java
@@ -1,12 +1,31 @@
 package Principal;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import Controlador.Controlador;
+import Modelo.ListaPeliculas;
+import Modelo.ListaTags;
+import Modelo.ListaValoraciones;
 
 public class Euskoflix {
 	
 	public static void main(String[] args) {
-        Controlador controlador = new Controlador();
-        controlador.iniciarAplicacion();
+        //Controlador controlador = new Controlador();
+        //controlador.iniciarAplicacion();
+		
+		//Poner id de peli y usuario a estimar
+		int idPeli=114;
+		int idUsu=1;
+		
+		try {
+			ArrayList<String> datosPelis=ListaPeliculas.getListaPeliculas().cargarDatosPelicula("C:\\Users\\mikel\\Desktop\\movie-titles.csv");
+			ArrayList<String> datosValoraciones=ListaValoraciones.cargarDatosValoracion("C:\\Users\\mikel\\Desktop\\movie-ratings.csv");
+			ArrayList<String> datosTags=ListaTags.getListaTags().getListaTags().cargarDatosTags("C:\\Users\\mikel\\Desktop\\movie-tags.csv");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		ListaValoraciones.getListaValoraciones().estimarValoracion(idPeli, idUsu);
     }
 
 }
